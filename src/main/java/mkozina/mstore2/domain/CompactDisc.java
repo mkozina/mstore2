0a43abff71769e26d6f1417a008ece62140ea29c
mkozina <mkozina@sigma.ug.edu.pl>
2016-01-18 00:45:17 +0100

    użycie JPA w modelu domeny

--- src/main/java/mkozina/mstore2/domain/CompactDisc.java
+++ src/main/java/mkozina/mstore2/domain/CompactDisc.java
@@ -1,5 +1,21 @@
 package mkozina.mstore2.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+
+@Entity
+@NamedQueries({
+	@NamedQuery(name = "compactDisc.all", query = "Select p from CompactDisc p")
+})
 public class CompactDisc{
 
 	private Long id;
@@ -7,55 +23,68 @@ public class CompactDisc{
 	private String album = "untitled";
 	private int year = 1900;
 	private int tracks = 99;
+	private Genre genre;
 
 	public CompactDisc(){
 	}
 
-	public CompactDisc(String artist, String album, int year, int tracks){
+	public CompactDisc(String artist, String album, int year, int tracks, Genre genre){
 		this.artist = artist;
 		this.album = album;
 		this.year = year;
 		this.tracks = tracks;
+		this.genre = genre;
 	}
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	public Long getId(){
 		return id;
 	}
-
 	public void setId(Long id){
 		this.id = id;
 	}
 
+	@Size(min = 1, max = 50)
 	public String getArtist(){
 		return artist;
 	}
-
 	public void setArtist(String artist){
 		this.artist = artist;
 	}
-	
+
+	@Size(min = 1, max = 50)
 	public String getAlbum(){
 		return album;
 	}
-
 	public void setAlbum(String album){
 		this.album = album;
 	}
 
+	@Min(1900)
 	public int getYear(){
 		return year;
 	}
-
 	public void setYear(int year){
 		this.year = year;
 	}
-	
+
+	@Min(1)
+	@Max(99)
 	public int getTracks(){
 		return tracks;
 	}
-
 	public void setTracks(int tracks){
 		this.tracks = tracks;
+	}
+
+	@ManyToOne
+	@JoinColumn(name = "genre_id", nullable = false)
+	public Genre getGenre(){
+		return genre;
+	}
+	public void setGenre(Genre genre){
+		this.genre = genre;
 	}
 
 }
--- src/main/java/mkozina/mstore2/domain/Genre.java
+++ src/main/java/mkozina/mstore2/domain/Genre.java
@@ -1,10 +1,30 @@
 package mkozina.mstore2.domain;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NamedQueries({
+	@NamedQuery(name = "genre.all", query = "Select p from Genre p")
+})
 public class Genre{
 
 	private Long id;
 	private String name = "";
 	private String description = "";
+
+	private List<CompactDisc> cds = new ArrayList<CompactDisc>();
 
 	public Genre(){
 	}
@@ -14,18 +34,19 @@ public class Genre{
 		this.description = description;
 	}
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	public Long getId(){
 		return id;
 	}
-
 	public void setId(Long id){
 		this.id = id;
 	}
 
+	@Size(min = 2, max = 20)
 	public String getName(){
 		return name;
 	}
-
 	public void setName(String name){
 		this.name = name;
 	}
@@ -33,9 +54,16 @@ public class Genre{
 	public String getDescription(){
 		return description;
 	}
-
 	public void setDescription(String description){
 		this.description = description;
+	}
+
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "genre")
+	public List<CompactDisc> getCompactDiscs() {
+		return cds;
+	}
+	public void setCompactDiscs(List<CompactDisc> cds) {
+		this.cds = cds;
 	}
 
 }
