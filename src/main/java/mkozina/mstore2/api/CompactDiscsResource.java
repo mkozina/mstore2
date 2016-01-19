397cf7ebb2d28a21775d665d9889fbe7b376f29a
mkozina <mkozina@sigma.ug.edu.pl>
2016-01-19 22:32:06 +0100

    poprawki w RESTful API

--- src/main/java/mkozina/mstore2/api/CompactDiscsResource.java
+++ src/main/java/mkozina/mstore2/api/CompactDiscsResource.java
@@ -1,9 +1,11 @@
 package mkozina.mstore2.api;
 
 import mkozina.mstore2.domain.CompactDisc;
-import mkozina.mstore2.service.CompactDiscStorage;
+import mkozina.mstore2.service.CompactDiscManager;
+import mkozina.mstore2.service.GenreManager;
 
 import javax.ejb.EJB;
+import javax.ejb.Stateless;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.MediaType;
@@ -11,61 +13,64 @@ import javax.print.attribute.standard.Media;
 
 import java.util.List;
 
+@Stateless
 @Path("cds")
 @Produces(MediaType.APPLICATION_JSON)
 public class CompactDiscsResource{
 
 	@EJB
-	CompactDiscStorage compactDiscStorage;
+	CompactDiscManager compactDiscManager;
+
+	@EJB
+	GenreManager genreManager;
 
 	@GET
 	public List<CompactDisc> getAllCompactDiscs(){
-		return compactDiscStorage.getAllCompactDiscs();
+		return compactDiscManager.getAllCompactDiscs();
 	}
 
 	@GET
 	@Path("{id}")
 	public CompactDisc get(@PathParam("id") Long id){
-		return compactDiscStorage.get(id);
+		return compactDiscManager.get(id);
 	}
 
 	@PUT
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	public void addCompactDisc(
+	public void add(
 		@FormParam("artist") String artist,
 		@FormParam("album") String album,
 		@FormParam("year") int year,
-		@FormParam("tracks") int tracks){
+		@FormParam("tracks") int tracks,
+		@FormParam("genreId") Long genreId){
 		CompactDisc newCompactDisc = new CompactDisc();
 		newCompactDisc.setArtist(artist);
 		newCompactDisc.setAlbum(album);
 		newCompactDisc.setYear(year);
 		newCompactDisc.setTracks(tracks);
-		compactDiscStorage.add(newCompactDisc);
+		newCompactDisc.setGenre(genreManager.get(genreId));
+		compactDiscManager.add(newCompactDisc);
 	}
 
 	@DELETE
 	@Path("{id}")
 	public void delete(@PathParam("id") Long id){
-		compactDiscStorage.delete(id);
+		compactDiscManager.delete(compactDiscManager.get(id));
 	}
 
 	@POST
-	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Path("{id}")
-	public void editCompactDisc(
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public void edit(
 		@PathParam("id") Long id,
 		@FormParam("artist") String artist,
 		@FormParam("album") String album,
 		@FormParam("year") int year,
-		@FormParam("tracks") int tracks){
-		CompactDisc compactDisc = compactDiscStorage.get(id);
+		@FormParam("tracks") int tracks,
+		@FormParam("genreId") Long genreId){
+		CompactDisc compactDisc = compactDiscManager.get(id);
 		if(compactDisc != null){
-			compactDisc.setArtist(artist);
-			compactDisc.setAlbum(album);
-			compactDisc.setYear(year);
-			compactDisc.setTracks(tracks);
-			compactDiscStorage.update(compactDisc);
+			compactDiscManager.update(compactDisc, artist, album, year, tracks, genreManager.get(genreId));
 		}
 	}
 
--- src/main/java/mkozina/mstore2/api/GenresResource.java
+++ src/main/java/mkozina/mstore2/api/GenresResource.java
@@ -1,9 +1,10 @@
 package mkozina.mstore2.api;
 
 import mkozina.mstore2.domain.Genre;
-import mkozina.mstore2.service.GenreStorage;
+import mkozina.mstore2.service.GenreManager;
 
 import javax.ejb.EJB;
+import javax.ejb.Stateless;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.MediaType;
@@ -11,53 +12,52 @@ import javax.print.attribute.standard.Media;
 
 import java.util.List;
 
+@Stateless
 @Path("genres")
 @Produces(MediaType.APPLICATION_JSON)
 public class GenresResource{
 
 	@EJB
-	GenreStorage genreStorage;
+	GenreManager genreManager;
 
 	@GET
 	public List<Genre> getAllGenres(){
-		return genreStorage.getAllGenres();
+		return genreManager.getAllGenres();
 	}
 
 	@GET
 	@Path("{id}")
 	public Genre get(@PathParam("id") Long id){
-		return genreStorage.get(id);
+		return genreManager.get(id);
 	}
 
 	@PUT
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	public void addGenre(
+	public void add(
 		@FormParam("name") String name,
 		@FormParam("description") String description){
 		Genre newGenre = new Genre();
 		newGenre.setName(name);
 		newGenre.setDescription(description);
-		genreStorage.add(newGenre);
+		genreManager.add(newGenre);
 	}
 
 	@DELETE
 	@Path("{id}")
 	public void delete(@PathParam("id") Long id){
-		genreStorage.delete(id);
+		genreManager.delete(genreManager.get(id));
 	}
 
 	@POST
-	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Path("{id}")
-	public void editGenre(
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public void edit(
 		@PathParam("id") Long id,
 		@FormParam("name") String name,
 		@FormParam("description") String description){
-		Genre genre = genreStorage.get(id);
+		Genre genre = genreManager.get(id);
 		if(genre != null){
-			genre.setName(name);
-			genre.setDescription(description);
-			genreStorage.update(genre);
+			genreManager.update(genre, name, description);
 		}
 	}
 
